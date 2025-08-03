ee28d4e30afea74ca1ed8e489eea17376b50d143
LeonUlcakar <67747860+LeonUlcakar@users.noreply.github.com>
2025-08-03 14:14:31 +0200

    last commit pre i2c freq change

--- Core/Src/nslp_dma.c
+++ Core/Src/nslp_dma.c
@@ -15,18 +15,26 @@ static struct Packet *tx_queue[TX_QUEUE_LENGTH];
 static uint8_t tx_head = 0, tx_tail = 0, tx_count = 0;
 static uint8_t tx_busy = 0;
 
-// Init function
+// RX state machine
+static enum {
+    RX_WAIT_START,
+    RX_HEADER,
+    RX_PAYLOAD,
+    RX_CRC
+} rx_state = RX_WAIT_START;
+
+static uint8_t rx_tmp[4 + MAX_PAYLOAD_SIZE] __attribute__((aligned(4)));
+static uint8_t rx_frame_start = 0;
+static uint8_t rx_payload_size = 0;
+
+static struct Packet rx_packet;
+
 void nslp_init(UART_HandleTypeDef *huart, CRC_HandleTypeDef *hcrc) {
     nslp_uart = huart;
     nslp_crc = hcrc;
 
-    __HAL_UART_ENABLE_IT(nslp_uart, UART_IT_IDLE);
-    HAL_StatusTypeDef r = HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-    if (r != HAL_OK) {
-        Error_Handler(); // or blink LED here
-    }
-    __HAL_DMA_DISABLE_IT(nslp_uart->hdmarx, DMA_IT_HT);
-
+    rx_state = RX_WAIT_START;
+    HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
 }
 
 void nslp_set_rx_callback(void (*callback)(struct Packet *)) {
@@ -75,113 +83,119 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
     start_tx();
 }
 
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size) {
-    if (huart != nslp_uart || size < FRAME_START_SIZE + HEADER_SIZE + CHECKSUM_SIZE) {
-        HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-        __HAL_DMA_DISABLE_IT(nslp_uart->hdmarx, DMA_IT_HT);
-        return;
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart != nslp_uart) return;
+
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);  // Debug indicator
+
+    switch (rx_state) {
+        case RX_WAIT_START:
+            if (rx_frame_start == FRAME_START) {
+                rx_state = RX_HEADER;
+                if (HAL_UART_Receive_DMA(nslp_uart, rx_tmp, 2) != HAL_OK) {
+                    rx_state = RX_WAIT_START;
+                    HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+                }
+            } else {
+            	rx_state = RX_WAIT_START;
+                HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+            }
+            break;
+
+        case RX_HEADER:
+            rx_packet.type = rx_tmp[0];
+            rx_payload_size = rx_tmp[1];
+
+            if (rx_payload_size > MAX_PAYLOAD_SIZE) {
+                rx_state = RX_WAIT_START;
+                HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+                return;
+            }
+
+            rx_packet.size = rx_payload_size;
+            rx_packet.payload = &rx_tmp[2];
+
+            rx_state = RX_PAYLOAD;
+            if (HAL_UART_Receive_DMA(nslp_uart, &rx_tmp[2], rx_payload_size) != HAL_OK) {
+                rx_state = RX_WAIT_START;
+                HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+            }
+            break;
+
+        case RX_PAYLOAD:
+            rx_state = RX_CRC;
+            if (HAL_UART_Receive_DMA(nslp_uart, &rx_tmp[2 + rx_payload_size], 4) != HAL_OK) {
+                rx_state = RX_WAIT_START;
+                HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+            }
+            break;
+
+        case RX_CRC: {
+            uint32_t received_crc;
+            memcpy(&received_crc, &rx_tmp[2 + rx_payload_size], 4);
+
+            __HAL_CRC_DR_RESET(nslp_crc);
+            uint32_t computed_crc = HAL_CRC_Calculate(nslp_crc, (uint32_t *)rx_tmp, 2 + rx_payload_size);
+
+            if (received_crc == computed_crc) {
+                if (rx_callback) {
+                    rx_callback(&rx_packet);
+                }
+            }
+
+            rx_state = RX_WAIT_START;
+            HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
+            break;
+        }
     }
-
-    if (rx_buffer[0] != FRAME_START) {
-        HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-        return;
-    }
-
-    uint8_t type = rx_buffer[1];
-    uint8_t payload_size = rx_buffer[2];
-
-    if (payload_size > MAX_PAYLOAD_SIZE || size < FRAME_START_SIZE + HEADER_SIZE + payload_size + CHECKSUM_SIZE) {
-        HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-        return;
-    }
-
-    uint32_t received_crc;
-    memcpy(&received_crc, &rx_buffer[3 + payload_size], 4);
-
-    __HAL_CRC_DR_RESET(nslp_crc);
-    uint32_t computed_crc = HAL_CRC_Calculate(nslp_crc, (uint32_t *)&rx_buffer[1], HEADER_SIZE + payload_size);
-    if (received_crc != computed_crc) {
-        HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-        return;
-    }
-
-    struct Packet pkt = {
-        .type = type,
-        .size = payload_size,
-        .payload = &rx_buffer[3]
-    };
-
-    if (rx_callback) {
-        rx_callback(&pkt);
-    }
-
-    HAL_UARTEx_ReceiveToIdle_DMA(nslp_uart, rx_buffer, MAX_PACKET_SIZE);
-    __HAL_DMA_DISABLE_IT(nslp_uart->hdmarx, DMA_IT_HT);
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
-    if (huart == nslp_uart) {
-        // Set a breakpoint or log error
-    	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-    }
+    if (huart != nslp_uart) return;
+
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);  // Error indicator
+    rx_state = RX_WAIT_START;
+    HAL_UART_Receive_DMA(nslp_uart, &rx_frame_start, 1);
 }
 
-
- uint8_t receiveData[HEADER_SIZE + 255 + CHECKSUM_SIZE];
-struct Packet receiveInstance = {
-	0x00,
-	0,
-	NULL
-};
-
+// Optional polling fallback
 struct Packet* receive_packet(UART_HandleTypeDef *huart, CRC_HandleTypeDef *hcrc) {
-	// Wait for frame start
-	uint8_t frameStart = 0;
-	if(HAL_UART_Receive_DMA(huart, &frameStart, FRAME_START_SIZE) != HAL_OK) {
-		return &receiveInstance;
-	}
+    static uint8_t receiveData[HEADER_SIZE + 255 + CHECKSUM_SIZE];
+    static struct Packet receiveInstance = { 0x00, 0, NULL };
 
-	if(frameStart != FRAME_START) {
-		return &receiveInstance;
-	}
+    uint8_t frameStart = 0;
+    if (HAL_UART_Receive(huart, &frameStart, FRAME_START_SIZE, 100) != HAL_OK) {
+        return &receiveInstance;
+    }
+    if (frameStart != FRAME_START) {
+        return &receiveInstance;
+    }
 
-	// Receive header
-	if(HAL_UART_Receive_DMA(huart, receiveData, HEADER_SIZE) != HAL_OK) {
-		return &receiveInstance;
-	}
+    if (HAL_UART_Receive(huart, receiveData, HEADER_SIZE, 100) != HAL_OK) {
+        return &receiveInstance;
+    }
 
-	receiveInstance.type = (char) receiveData[0];
-	receiveInstance.size = receiveData[1];
+    receiveInstance.type = (char) receiveData[0];
+    receiveInstance.size = receiveData[1];
 
-	if (receiveInstance.size == 0) {
-		return &receiveInstance;
-	}
+    if (receiveInstance.size == 0) {
+        return &receiveInstance;
+    }
 
-	// Read data
-	if(HAL_UART_Receive_DMA(huart, &receiveData[HEADER_SIZE], receiveInstance.size) != HAL_OK) {
-		return &receiveInstance;
-	}
-	receiveInstance.payload = &receiveData[HEADER_SIZE];
+    if (HAL_UART_Receive(huart, &receiveData[HEADER_SIZE], receiveInstance.size, 100) != HAL_OK) {
+        return &receiveInstance;
+    }
+    receiveInstance.payload = &receiveData[HEADER_SIZE];
 
-	// Read checksum
-	if(HAL_UART_Receive_DMA(huart, &receiveData[HEADER_SIZE + receiveInstance.size], CHECKSUM_SIZE) != HAL_OK) {
-		return &receiveInstance;
-	}
+    if (HAL_UART_Receive(huart, &receiveData[HEADER_SIZE + receiveInstance.size], CHECKSUM_SIZE, 100) != HAL_OK) {
+        return &receiveInstance;
+    }
 
-	// Perform checksum
-	uint32_t calculatedCrc = HAL_CRC_Calculate(
-		hcrc,
-		(uint32_t *) receiveData,
-		HEADER_SIZE + receiveInstance.size
-	);
+    uint32_t calculatedCrc = HAL_CRC_Calculate(hcrc, (uint32_t *) receiveData, HEADER_SIZE + receiveInstance.size);
+    uint32_t receivedCrc = *(uint32_t *) (&receiveData[HEADER_SIZE + receiveInstance.size]);
+    if (receivedCrc != calculatedCrc) {
+        return &receiveInstance;
+    }
 
-	uint32_t receivedCrc = *(uint32_t *) (&receiveData[HEADER_SIZE + receiveInstance.size]);
-	if (receivedCrc != calculatedCrc) {
-		return &receiveInstance;
-	}
-
-	return &receiveInstance;
+    return &receiveInstance;
 }
-
-
-
